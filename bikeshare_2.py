7a849e7e118353a13ec63e41a9be739f32feb8ac
ej-hailey <ejlee.hailey@gmail.com>
2022-10-27 23:43:58 -0400

    Additional comments added

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -2,10 +2,12 @@ import time
 import pandas as pd
 import numpy as np
 
+# dictionary of cities and respective files
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+# set up global variables
 city = ""
 month = ""
 day = ""
